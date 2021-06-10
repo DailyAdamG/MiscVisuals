c80512f3c90f0630d085a19346d388a96880b767
Adam <daily.adam@gmail.com>
2021-06-10 00:05:33 -0500

    New visuals

---
+++ Website Visualizations/Most Times Reaching X Streak/MLB/Most Consecutive Seasons with 30 or More Home Runs.R
@@ -0,0 +1,131 @@
+#Set working directory
+
+setwd("C:/Users/daily/Desktop/Repositories/MiscVisuals/Website Visualizations/Most Times Reaching X Streak/MLB")
+
+#Loading necessary libraries
+
+library(sqldf)
+library(tidyverse)
+library(zoo)
+library(data.table)
+library(Lahman)
+
+#Get data from Lahman Database
+
+data <- sqldf(' WITH sub1 AS(SELECT People.playerID, Batting.yearID, People.nameFirst, People.nameLast,
+GROUP_CONCAT(TeamsFranchises.franchName) AS Teams, TeamsFranchises.franchID, SUM(Batting.HR) AS HR,
+RANK () OVER(PARTITION BY People.playerID ORDER BY Batting.yearID) AS SeasonNumber
+                    FROM People
+                    INNER JOIN Batting
+                    ON People.playerID = Batting.playerID
+                    INNER JOIN Teams
+                    ON Batting.teamID = Teams.teamID
+                    AND Batting.yearID = Teams.yearID
+                    INNER JOIN TeamsFranchises
+                    ON Teams.franchID = TeamsFranchises.franchID
+                    GROUP BY Batting.playerID, Batting.yearID),
+              sub2 AS (SELECT *,
+              SeasonNumber - RANK() OVER(PARTITION BY playerID ORDER BY yearID) AS streakID
+              FROM sub1
+              WHERE HR >= 30)
+              SELECT MIN(yearID) AS MIN,
+              MAX(yearID) AS MAX,
+              nameFirst,
+              nameLast,
+              GROUP_CONCAT(DISTINCT Teams) AS Teams,
+              COUNT(DISTINCT Teams) AS Franchises,
+              COUNT(streakID) AS Times
+              FROM sub2
+              GROUP BY playerID, streakID
+              ORDER BY Times DESC')
+
+################################################################################
+
+#Doing the same thing as earlier but using R instead of SQL
+
+fun_data <- sqldf('SELECT People.playerID, Batting.yearID, People.nameFirst, People.nameLast,
+GROUP_CONCAT(TeamsFranchises.franchName) AS Teams, TeamsFranchises.franchID, SUM(Batting.HR) AS HR
+FROM People
+INNER JOIN Batting
+ON People.playerID = Batting.playerID
+INNER JOIN Teams
+ON Batting.teamID = Teams.teamID
+AND Batting.yearID = Teams.yearID
+INNER JOIN TeamsFranchises
+ON Teams.franchID = TeamsFranchises.franchID
+GROUP BY Batting.playerID, Batting.yearID')
+
+fun_data <- fun_data %>%
+  arrange(playerID, yearID) %>%
+  group_by(playerID) %>%
+  mutate(SeasonNumber = rank(yearID)) %>%
+  filter(HR >= 30) %>%
+  mutate(HRList = rank(yearID)) %>% 
+  mutate(streakID = SeasonNumber - HRList)
+  
+fun_data <- aggregate(x = fun_data$HR, by = list(fun_data$playerID, fun_data$nameFirst, fun_data$nameLast,
+                                     fun_data$streakID), FUN = length) %>%
+  arrange(-x)
+
+#################################################################################
+
+#Combine Columns and Filter Data
+
+top_data <- data %>%
+  mutate(Rank = rank(-Times, ties.method = "min"))  %>%
+  filter(Rank <= 10) %>%
+  mutate(DisplayFranchises = ifelse(Franchises != 1, "Multiple Teams", Teams)) %>%
+  mutate(chart_label = paste(nameFirst, nameLast, "\n", MIN, "-", MAX, "\n", DisplayFranchises))
+
+#Team Colors for display
+
+fill_color <- c("Arizona Diamondbacks" = "#5F259F",
+                "Atlanta Braves" = "#CE1141",
+                "Baltimore Orioles" = "#DF4601",
+                "Boston Red Sox" = "#BD3039",
+                "Chicago Cubs" = "#0E3386",
+                "Chicago White Sox" = "#27251F",
+                "Cincinnati Reds" = "#C6011F",
+                "Cleveland Indians" = "#E31937",
+                "Colorado Rockies" = "#33006F;",
+                "Detroit Tigers" = "#FA4616",
+                "Houston Astros" = "#EB6E1F",
+                "Kansas City Royals" = "#004687",
+                "Los Angeles Angels" = "#BA0021",
+                "Los Angeles Dodgers" = "#005A9C",
+                "Miami Marlins" = "#00A3E0",
+                "Milwaukee Brewers" = "#FFC52F",
+                "Minnesota Twins" = "#002B5C",
+                "New York Mets" = "#FF5910",
+                "New York Yankees" = "#0C2340",
+                "Oakland Athletics" = "#003831",
+                "Philadelphia Phillies" = "#E81828",
+                "Pittsburgh Pirates" = "#FDB827",
+                "St. Louis Cardinals" = "#C41E3A",
+                "San Diego Padres" = "#2F241D",
+                "San Francisco Giants" = "#FD5A1E",
+                "Seattle Mariners" = "#005C5C",
+                "Tampa Bay Rays" = "#8FBCE6",
+                "Texas Rangers" = "#C0111F",
+                "Toronto Blue Jays" = "#134A8E",
+                "Washington Nationals" = "#AB0003",
+                "Multiple Teams" = "seashell")
+
+#Create visual
+
+top_data %>%
+  ggplot(aes(x = reorder(chart_label, Times), y = Times, fill = DisplayFranchises)) +
+  geom_bar(stat = "identity", color = "black") +
+  geom_text(aes(label = Times), fontface = "bold", hjust = -0.3) +
+  coord_flip(ylim = c(0, 15)) +
+  scale_y_continuous(breaks = seq(0, 15, 1)) +
+  scale_fill_manual(values = fill_color) +
+  theme(plot.title = element_text(size = 16, face = "bold"),
+        plot.subtitle = element_text(size = 12),
+        axis.title.x = element_text(size = 12, face = "bold"),
+        axis.text.x = element_text(size = 10),
+        axis.text.y = element_text(size = 10, face ="bold"),
+        legend.position = "none") +
+  labs(title = "Most Consecutive Seasons with 30 or More Home Runs",
+       x = "",
+       y = "Consecutive 30+ Home Run Seasons")
---
+++ Website Visualizations/Most X in Span to Start Career/MLB/Most HR in 5 Season Span to Start Career.R
@@ -0,0 +1,148 @@
+#Set working directory
+
+setwd("C:/Users/daily/Desktop/Repositories/MiscVisuals/Website Visualizations/Most X in Span to Start Career/MLB")
+
+#Loading necessary libraries
+
+library(sqldf)
+library(tidyverse)
+library(zoo)
+library(data.table)
+library(Lahman)
+
+#Get data from Lahman Database
+
+data <- sqldf('SELECT People.playerID, Batting.yearID, People.nameFirst, People.nameLast,
+GROUP_CONCAT(TeamsFranchises.franchName) AS Teams, TeamsFranchises.franchID, SUM(Batting.HR) AS HR
+                    FROM People
+                    INNER JOIN Batting
+                    ON People.playerID = Batting.PlayerID
+                    INNER JOIN Teams
+                    ON Batting.teamID = Teams.teamID
+                    AND Batting.yearID = Teams.yearID
+                    INNER JOIN TeamsFranchises
+                    ON Teams.franchID = TeamsFranchises.franchID
+                    GROUP BY Batting.playerID, Batting.yearID
+                    ORDER BY SUM(Batting.HR) DESC')
+
+#Calculate the totals and find the season the span started
+
+data <- data %>%
+  arrange(playerID, yearID) %>%
+  group_by(playerID) %>%
+  mutate(HR_Total = rollapply(HR, width = 5, FUN = sum, align = "left",partial = TRUE)) %>%
+  mutate(firstYear = lead(yearID, 1)) %>%
+  mutate(secondYear = lead(yearID,2)) %>%
+  mutate(thirdYear = lead(yearID,3)) %>%
+  mutate(fourthYear = lead(yearID,4)) %>%
+  mutate(Range = fourthYear - yearID) %>%
+  mutate(firstYearTeam = lead(Teams,1)) %>%
+  mutate(secondYearTeam = lead(Teams,2)) %>%
+  mutate(thirdYearTeam = lead(Teams,3)) %>%
+  mutate(fourthYearTeam = lead(Teams,4)) %>%
+  mutate(OneFranchise = ifelse(Teams == firstYearTeam & Teams == secondYearTeam & Teams == thirdYearTeam & Teams == fourthYearTeam, "Yes", "No")) %>%
+  ungroup()
+
+#Choose only the columns needed for visual
+
+display_data <- sqldf('SELECT MIN(yearID) AS MIN,
+      fourthYear AS MAX,
+      Range,
+      playerID,
+      nameFirst,
+      nameLast,
+      Teams,
+      HR_Total,
+      OneFranchise
+      FROM data
+      GROUP BY playerID
+      ORDER BY HR_Total DESC')
+
+
+#Limit to top 10 spots
+
+top_data <- display_data %>%
+  mutate(Rank = rank(-HR_Total, ties.method = "min")) %>%
+  filter(Rank <= 10)
+
+#Create Team Name for Players with More Than 1 Franchise
+
+top_data <- top_data %>%
+  mutate(DisplayFranchises = ifelse(OneFranchise == "No", "Multiple Teams", Teams))
+
+#Combine columns for display
+
+top_data <- top_data %>%
+  mutate(chart_label = paste(nameFirst,nameLast, "\n", MIN, "-", MAX, "\n",DisplayFranchises))
+
+#Team Colors for display
+
+fill_color <- c("Arizona Diamondbacks" = "#5F259F",
+                "Atlanta Braves" = "#CE1141",
+                "Baltimore Orioles" = "#DF4601",
+                "Boston Red Sox" = "#BD3039",
+                "Chicago Cubs" = "#0E3386",
+                "Chicago White Sox" = "#27251F",
+                "Cincinnati Reds" = "#C6011F",
+                "Cleveland Indians" = "#E31937",
+                "Colorado Rockies" = "#33006F;",
+                "Detroit Tigers" = "#FA4616",
+                "Houston Astros" = "#EB6E1F",
+                "Kansas City Royals" = "#004687",
+                "Los Angeles Angels" = "#BA0021",
+                "Los Angeles Dodgers" = "#005A9C",
+                "Miami Marlins" = "#00A3E0",
+                "Milwaukee Brewers" = "#FFC52F",
+                "Minnesota Twins" = "#002B5C",
+                "New York Mets" = "#FF5910",
+                "New York Yankees" = "#0C2340",
+                "Oakland Athletics" = "#003831",
+                "Philadelphia Phillies" = "#E81828",
+                "Pittsburgh Pirates" = "#FDB827",
+                "St. Louis Cardinals" = "#C41E3A",
+                "San Diego Padres" = "#2F241D",
+                "San Francisco Giants" = "#FD5A1E",
+                "Seattle Mariners" = "#005C5C",
+                "Tampa Bay Rays" = "#8FBCE6",
+                "Texas Rangers" = "#C0111F",
+                "Toronto Blue Jays" = "#134A8E",
+                "Washington Nationals" = "#AB0003",
+                "Multiple Teams" = "seashell")
+
+#Create visual
+
+#Create visual
+
+top_data %>%
+  ggplot(aes(x = reorder(chart_label, HR_Total), y = HR_Total, fill = DisplayFranchises)) +
+  geom_bar(stat = "identity", color = "black") +
+  geom_text(aes(label = HR_Total), fontface = "bold", hjust = -0.3) +
+  coord_flip(ylim = c(80, 120)) +
+  scale_y_continuous(breaks = seq(80, 120, 5)) +
+  scale_fill_manual(values = fill_color) +
+  theme(plot.title = element_text(size = 16, face = "bold"),
+        plot.subtitle = element_text(size = 12),
+        axis.title.x = element_text(size = 12, face = "bold"),
+        axis.text.x = element_text(size = 10),
+        axis.text.y = element_text(size = 10, face ="bold"),
+        legend.position = "none") +
+  labs(title = "Most Home Runs in a 3 Season Span \nto Start Career",
+       x = "",
+       y = "Home Runs")
+
+top_data %>%
+  ggplot(aes(x = reorder(chart_label, HR_Total), y = HR_Total, fill = DisplayFranchises)) +
+  geom_bar(stat = "identity", color = "black") +
+  geom_text(aes(label = HR_Total), fontface = "bold", hjust = -0.3) +
+  coord_flip(ylim = c(150, 220)) +
+  scale_y_continuous(breaks = seq(150, 220, 10)) +
+  scale_fill_manual(values = fill_color) +
+  theme(plot.title = element_text(size = 16, face = "bold"),
+        plot.subtitle = element_text(size = 12),
+        axis.title.x = element_text(size = 12, face = "bold"),
+        axis.text.x = element_text(size = 10),
+        axis.text.y = element_text(size = 10, face ="bold"),
+        legend.position = "none") +
+  labs(title = "Most Home Runs in a 5 Season Span \nto Start Career",
+       x = "",
+       y = "Home Runs")
