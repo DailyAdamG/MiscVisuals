92f19b27e58b146c39b2d559a9682db29bd37759
Adam <daily.adam@gmail.com>
2021-08-08 12:36:03 -0500

    New files

---
+++ Website Visualizations/Most Times Reaching X Streak/Cubs/Most Consecutive Seasons with 25+ HR and 100+ RBI Cubs.R
@@ -0,0 +1,107 @@
+#Set working directory
+
+setwd("C:/Users/daily/Desktop/Repositories/MiscVisuals/Website Visualizations/Most Times Reaching X Streak/Cubs")
+
+#Loading necessary libraries
+
+library(sqldf)
+library(tidyverse)
+library(zoo)
+library(data.table)
+library(Lahman)
+
+#Get data from Lahman Database
+
+data <- sqldf(" WITH sub1 AS(SELECT People.playerID, Batting.yearID, People.nameFirst, People.nameLast,
+TeamsFranchises.franchID, SUM(Batting.HR) AS HR, SUM(Batting.RBI) AS RBI,
+RANK () OVER(PARTITION BY People.playerID ORDER BY Batting.yearID) AS SeasonNumber
+                    FROM People
+                    INNER JOIN Batting
+                    ON People.playerID = Batting.playerID
+                    INNER JOIN Teams
+                    ON Batting.teamID = Teams.teamID
+                    AND Batting.yearID = Teams.yearID
+                    INNER JOIN TeamsFranchises
+                    ON Teams.franchID = TeamsFranchises.franchID
+                    WHERE TeamsFranchises.franchName = 'Chicago Cubs'
+                    GROUP BY Batting.playerID, Batting.yearID),
+              sub2 AS (SELECT *,
+              SeasonNumber - RANK() OVER(PARTITION BY playerID ORDER BY yearID) AS streakID
+              FROM sub1
+              WHERE HR >= 25
+              AND RBI >= 100)
+              SELECT MIN(yearID) AS MIN,
+              MAX(yearID) AS MAX,
+              nameFirst,
+              nameLast,
+              COUNT(streakID) AS Times
+              FROM sub2
+              GROUP BY playerID, streakID
+              ORDER BY Times DESC")
+
+#Combine Columns and Filter Data
+
+top_data <- data %>%
+  mutate(Rank = rank(-Times, ties.method = "min"))  %>%
+  filter(Times >= 2) %>%
+  mutate(fullName = paste(nameFirst, nameLast)) %>%
+  mutate(chart_label = paste(nameFirst, nameLast, "\n", MIN, "-", MAX)) %>%
+  arrange(-Times, desc(fullName), -MIN)
+
+#Create a column for alternating row colors in graph
+
+top_data <- top_data %>%
+  mutate(EvenOdd = ifelse(row_number() %% 2 == 1, "Odd", "Even"))
+
+#Team Colors for display
+
+fill_color <- c("Arizona Diamondbacks" = "#5F259F",
+                "Atlanta Braves" = "#CE1141",
+                "Baltimore Orioles" = "#DF4601",
+                "Boston Red Sox" = "#BD3039",
+                "Chicago Cubs" = "#0E3386",
+                "Chicago White Sox" = "#27251F",
+                "Cincinnati Reds" = "#C6011F",
+                "Cleveland Indians" = "#E31937",
+                "Colorado Rockies" = "#33006F",
+                "Detroit Tigers" = "#FA4616",
+                "Houston Astros" = "#EB6E1F",
+                "Kansas City Royals" = "#004687",
+                "Los Angeles Angels" = "#BA0021",
+                "Los Angeles Dodgers" = "#005A9C",
+                "Miami Marlins" = "#00A3E0",
+                "Milwaukee Brewers" = "#FFC52F",
+                "Minnesota Twins" = "#002B5C",
+                "New York Mets" = "#FF5910",
+                "New York Yankees" = "#0C2340",
+                "Oakland Athletics" = "#003831",
+                "Philadelphia Phillies" = "#E81828",
+                "Pittsburgh Pirates" = "#FDB827",
+                "St. Louis Cardinals" = "#C41E3A",
+                "San Diego Padres" = "#2F241D",
+                "San Francisco Giants" = "#FD5A1E",
+                "Seattle Mariners" = "#005C5C",
+                "Tampa Bay Rays" = "#8FBCE6",
+                "Texas Rangers" = "#C0111F",
+                "Toronto Blue Jays" = "#134A8E",
+                "Washington Nationals" = "#AB0003",
+                "Multiple Teams" = "seashell")
+
+#Create visual
+
+top_data %>%
+  ggplot(aes(x = reorder(chart_label, Times), y = Times, fill = EvenOdd)) +
+  geom_bar(stat = "identity", color = "black") +
+  geom_text(aes(label = Times), fontface = "bold", hjust = -0.3) +
+  coord_flip(ylim = c(0, 10)) +
+  scale_y_continuous(breaks = seq(0, 10, 1)) +
+  scale_fill_manual(values = c("Even" = "#0E3386", "Odd" = "#CC3433")) +
+  theme(plot.title = element_text(size = 16, face = "bold"),
+        plot.subtitle = element_text(size = 12),
+        axis.title.x = element_text(size = 12, face = "bold"),
+        axis.text.x = element_text(size = 10),
+        axis.text.y = element_text(size = 10, face ="bold"),
+        legend.position = "none") +
+  labs(title = "Most Consecutive Seasons with \n25+ Home Runs and 100+ RBI as a Cub",
+       x = "",
+       y = "Consecutive 25+ Home Run and 100+ RBI Seasons")
