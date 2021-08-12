dfb8828d74cca56d2eff95198dffcec8454bfdcf
Adam <daily.adam@gmail.com>
2021-08-12 11:53:47 -0500

    New files

---
+++ Animated Line Charts/Judge Comp.R
@@ -0,0 +1,52 @@
+#Set working directory
+
+setwd("C:/Users/daily/Desktop/Repositories/MiscVisuals/Animated Line Charts")
+
+#Loading libraries
+
+library(tidyverse)
+library(gganimate)
+library(hrbrthemes)
+library(weights)
+
+#Import data
+
+data <- read.csv("C:/Users/daily/Desktop/Repositories/MiscVisuals/Animated Line Charts/Judge Comp.csv")
+
+#Get running WAR
+
+data <- data %>%
+  arrange(Name, Season) %>%
+  group_by(Name) %>%
+  mutate(CareerWAR = cumsum(WAR))
+
+#Create color scheme
+
+cols <- c("Brian Giles" = "#473729", "Leon Wagner" = "#E31937", "Jim Gentile" = "#DF4601",
+          "Norm Cash" = "#0C2340", "Craig Wilson" = "#FDB827", "Aaron Judge" = "#C4CED3")
+
+#Create visual
+
+p <- data %>%
+  ggplot(aes(x = Age, y = CareerWAR, group = Name, color = Name)) +
+  geom_line(size = 2) +
+  geom_point(size = 4) +
+  #geom_text(aes(label = rd(CareerWAR, digits = 1)), color = "white", fontface = "bold", size = 12/.pt, hjust = .5, vjust = .5) +
+  scale_color_manual(values = cols) +
+  scale_x_continuous(breaks = seq(23, 39, 1)) +
+  scale_y_continuous(breaks = seq(0, 60, 5)) +
+  hrbrthemes::theme_ipsum(plot_title_size = 24, base_size = 16) +
+  theme(panel.grid.major.y=element_blank(),
+        panel.grid.minor.x=element_blank(),
+        plot.margin = margin(1,1,1,2,"cm"),
+        axis.text.x = element_text(angle = 65)) +
+  transition_reveal(Age) +
+  ease_aes("linear") +
+  view_follow() +
+  labs(title = "Aaron Judge WAR vs. Top 5 Bill James Similarity Scores", subtitle = "Only Includes Players Who Have Finished Their Career", x = "Season Age", y = "Career WAR")
+
+#Animate the visual
+
+anim <- animate(p, nframes = 22, fps = 1, width = 800, height = 482, end_pause = 5, renderer = magick_renderer())
+
+anim_save("Judge Comp.gif", anim)
