5513e9213b2e1cebcb540d4183ff6db8695a318e
Adam <daily.adam@gmail.com>
2021-08-18 14:37:24 -0500

    New files

---
+++ Most Hits by a One Franchise Player.R
@@ -0,0 +1,94 @@
+#Set working directory
+
+setwd("C:/Users/daily/Desktop/Repositories/MiscVisuals")
+
+#Loading necessary libraries
+
+library(sqldf)
+library(tidyverse)
+library(zoo)
+library(data.table)
+library(Lahman)
+
+#Get data from Lahman Database
+
+data <- sqldf("SELECT People.playerID, People.nameFirst, People.nameLast,
+TeamsFranchises.franchName AS Team, TeamsFranchises.franchID, SUM(Batting.H) AS H,
+MIN(Batting.yearID) AS MIN, MAX(Batting.yearID) AS MAX, COUNT(DISTINCT TeamsFranchises.franchID) AS Franchises
+                    FROM People
+                    INNER JOIN Batting
+                    ON People.playerID = Batting.PlayerID
+                    INNER JOIN Teams
+                    ON Batting.teamID = Teams.teamID
+                    AND Batting.yearID = Teams.yearID
+                    INNER JOIN TeamsFranchises
+                    ON Teams.franchID = TeamsFranchises.franchID
+                    GROUP BY Batting.playerID
+                    HAVING Franchises = 1
+                    ORDER BY SUM(Batting.H) DESC")
+
+#Limit to top 10 spots
+
+top_data <- data %>%
+  mutate(Rank = rank(-H, ties.method = "min")) %>%
+  filter(Rank <= 10)
+
+
+#Combine columns for display
+
+top_data <- top_data %>%
+  mutate(chart_label = paste(nameFirst,nameLast,"\n",MIN,"-",MAX,"\n",Team))
+
+#Team Colors for display
+
+fill_color <- c("Arizona Diamondbacks" = "#5F259F",
+                "Atlanta Braves" = "#CE1141",
+                "Baltimore Orioles" = "#DF4601",
+                "Boston Red Sox" = "#BD3039",
+                "Chicago Cubs" = "#0E3386",
+                "Chicago White Sox" = "#27251F",
+                "Cincinnati Reds" = "#C6011F",
+                "Cleveland Indians" = "#0C2340",
+                "Cleveland Spiders" = "#0C2340",
+                "Colorado Rockies" = "#33006F",
+                "Detroit Tigers" = "#FA4616",
+                "Houston Astros" = "#EB6E1F",
+                "Kansas City Royals" = "#004687",
+                "Los Angeles Angels" = "#BA0021",
+                "Los Angeles Dodgers" = "#005A9C",
+                "Miami Marlins" = "#00A3E0",
+                "Milwaukee Brewers" = "#FFC52F",
+                "Minnesota Twins" = "#002B5C",
+                "New York Mets" = "#FF5910",
+                "New York Yankees" = "#0C2340",
+                "Oakland Athletics" = "#003831",
+                "Philadelphia Phillies" = "#E81828",
+                "Pittsburgh Pirates" = "#FDB827",
+                "St. Louis Cardinals" = "#C41E3A",
+                "San Diego Padres" = "#2F241D",
+                "San Francisco Giants" = "#FD5A1E",
+                "Seattle Mariners" = "#005C5C",
+                "Tampa Bay Rays" = "#8FBCE6",
+                "Texas Rangers" = "#C0111F",
+                "Toronto Blue Jays" = "#134A8E",
+                "Washington Nationals" = "#AB0003",
+                "Multiple Franchises" = "seashell")
+
+#Create visual
+
+top_data %>%
+  ggplot(aes(x = reorder(chart_label, H), y = H, fill = Team)) +
+  geom_bar(stat = "identity", color = "black") +
+  geom_text(aes(label = H), fontface = "bold", hjust = -0.3) +
+  coord_flip(ylim = c(3000, 3700)) +
+  scale_y_continuous(breaks = seq(3000, 3700, 50)) +
+  scale_fill_manual(values = fill_color) +
+  theme(plot.title = element_text(size = 16, face = "bold"),
+        plot.subtitle = element_text(size = 12),
+        axis.title.x = element_text(size = 12, face = "bold"),
+        axis.text.x = element_text(size = 10),
+        axis.text.y = element_text(size = 10, face ="bold"),
+        legend.position = "none") +
+  labs(title = "Most Hits by a Player in MLB History\nWho Only Played for One Franchise",
+       x = "",
+       y = "Hits")
